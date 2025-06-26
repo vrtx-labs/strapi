798a0ca9f8987d695c1619055f6aef8672525403
Philipp Bontemps <philipp@vrtxlabs.com>
2025-06-26 14:23:32 +0200

    chore: vendor packages/core/admin/dist
    For easier deployment, we vendor the resulting
    `packages/core/admin/dist` directory of the command `yarn setup`, as per
    the development instructions. updated for v4.25.22

---
+++ packages/core/admin/dist/_chunks/constants-aTiwONjV.mjs
@@ -0,0 +1,52 @@
+const ROUTES_EE = [
+  ...window.strapi.features.isEnabled(window.strapi.features.AUDIT_LOGS) ? [
+    {
+      async Component() {
+        const { ProtectedListPage } = await import("./ListPage-5BAUKuC5.mjs");
+        return ProtectedListPage;
+      },
+      to: "/settings/audit-logs",
+      exact: true
+    }
+  ] : [],
+  ...window.strapi.features.isEnabled(window.strapi.features.REVIEW_WORKFLOWS) ? [
+    {
+      async Component() {
+        const { ProtectedReviewWorkflowsPage } = await import("./ListPage-cHUafRyk.mjs");
+        return ProtectedReviewWorkflowsPage;
+      },
+      to: "/settings/review-workflows",
+      exact: true
+    },
+    {
+      async Component() {
+        const { ReviewWorkflowsCreatePage } = await import("./CreatePage-qJ83w6V7.mjs");
+        return ReviewWorkflowsCreatePage;
+      },
+      to: "/settings/review-workflows/create",
+      exact: true
+    },
+    {
+      async Component() {
+        const { ReviewWorkflowsEditPage } = await import("./EditPage-4-Stfks4.mjs");
+        return ReviewWorkflowsEditPage;
+      },
+      to: "/settings/review-workflows/:workflowId",
+      exact: true
+    }
+  ] : [],
+  ...window.strapi.features.isEnabled(window.strapi.features.SSO) ? [
+    {
+      async Component() {
+        const { ProtectedSSO } = await import("./SingleSignOnPage-CWotQVZe.mjs");
+        return ProtectedSSO;
+      },
+      to: "/settings/single-sign-on",
+      exact: true
+    }
+  ] : []
+];
+export {
+  ROUTES_EE
+};
+//# sourceMappingURL=constants-aTiwONjV.mjs.map
