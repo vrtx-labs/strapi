35fd3823dfad35bc6d49f259d7267257d3fd31b1
Leonie Pormann <lporrmann@vrtxlabs.local>
2025-11-20 15:54:42 +0100

    Commit missing files

---
+++ packages/core/admin/dist/_chunks/constants-hDlDfGp8.mjs
@@ -0,0 +1,52 @@
+const ROUTES_EE = [
+  ...window.strapi.features.isEnabled(window.strapi.features.AUDIT_LOGS) ? [
+    {
+      async Component() {
+        const { ProtectedListPage } = await import("./ListPage-4xBFFEdC.mjs");
+        return ProtectedListPage;
+      },
+      to: "/settings/audit-logs",
+      exact: true
+    }
+  ] : [],
+  ...window.strapi.features.isEnabled(window.strapi.features.REVIEW_WORKFLOWS) ? [
+    {
+      async Component() {
+        const { ProtectedReviewWorkflowsPage } = await import("./ListPage-qXQYwRUJ.mjs");
+        return ProtectedReviewWorkflowsPage;
+      },
+      to: "/settings/review-workflows",
+      exact: true
+    },
+    {
+      async Component() {
+        const { ReviewWorkflowsCreatePage } = await import("./CreatePage-YqgRobjy.mjs");
+        return ReviewWorkflowsCreatePage;
+      },
+      to: "/settings/review-workflows/create",
+      exact: true
+    },
+    {
+      async Component() {
+        const { ReviewWorkflowsEditPage } = await import("./EditPage-32Ytc4u8.mjs");
+        return ReviewWorkflowsEditPage;
+      },
+      to: "/settings/review-workflows/:workflowId",
+      exact: true
+    }
+  ] : [],
+  ...window.strapi.features.isEnabled(window.strapi.features.SSO) ? [
+    {
+      async Component() {
+        const { ProtectedSSO } = await import("./SingleSignOnPage-dWZXMUgt.mjs");
+        return ProtectedSSO;
+      },
+      to: "/settings/single-sign-on",
+      exact: true
+    }
+  ] : []
+];
+export {
+  ROUTES_EE
+};
+//# sourceMappingURL=constants-hDlDfGp8.mjs.map
